1f6f9f1e58154bb5a8af5f986eba34c977ca8d6d
Joel Wembo <joelotepawembo@gmail.com>
2021-05-10 07:15:55 +0800

    Create SQL Statement

---
+++ MySQL/SQLFunction.sql
@@ -0,0 +1,7 @@
+# in-built functions and commands to access and manage databases according to our requirements.
+
+/* Function to retrive student name as lower case*/
+sqlstmnt = "SELECT UCASE(StudentName)
+FROM Students;"  
+/* Function to find len*/
+sqlstmnt = "SELECT LENGTH(String) AS SampleColumn; "
