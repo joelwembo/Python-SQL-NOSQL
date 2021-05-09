ad27154de95d6064e54d09d2fa783eb18000a833
Joel Wembo <joelotepawembo@gmail.com>
2021-05-10 07:24:37 +0800

    View Statement syntax

--- MySQL/SQLFunction.sql
+++ MySQL/SQLFunction.sql
@@ -1,7 +1,8 @@
 # in-built functions and commands to access and manage databases according to our requirements.
-
 /* Function to retrive student name as lower case*/
 sqlstmnt = "SELECT UCASE(StudentName)
 FROM Students;"  
 /* Function to find len*/
 sqlstmnt = "SELECT LENGTH(String) AS SampleColumn; "
+
+sqlstmnt = "SELECT MID(ColumnName, Start, Length) FROM TableName;"
