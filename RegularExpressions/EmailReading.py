dfae0a4fd052167b1570497f5213a81484430866
Joel Wembo <joelotepawembo@gmail.com>
2021-05-12 19:43:10 +0800

    Updated the regular expressions scripts

--- RegularExpressions/EmailReading.py
+++ RegularExpressions/EmailReading.py
@@ -1,7 +1,7 @@
 import re  
 import pandas as pd   
 
-inputData = "joe has sent an invoice email to joelotepawembo@gmail.com by using his email id fabrice@gmail.com and he also oss boss@amazon.co.uk"
+inputData = "joe with +638923782378  +63278239923 has sent an invoice email to joelotepawembo@gmail.com by using his email id fabrice@gmail.com and he also oss boss@amazon.co.uk"
 findEmail = re.findall(r'[\w\.-]+@[\w\.-]+', inputData)
 
-print(findEmail)
+print("Emails are : " , findEmail)
