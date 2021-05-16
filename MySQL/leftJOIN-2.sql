5667f4f7d6e26535c0a5ec263d2251f703d6b2b2
Joel Wembo <joelotepawembo@gmail.com>
2021-05-16 23:22:34 +0800

    SQL Left Join update

---
+++ MySQL/leftJOIN-2.sql
@@ -0,0 +1,9 @@
+/***Syntax
+The basic syntax of a LEFT JOIN is as follows.
+**/
+
+SELECT table1.column1, table2.column2...
+FROM table1
+LEFT JOIN table2
+ON table1.common_field = table2.common_field;
+
