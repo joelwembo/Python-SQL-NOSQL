a4ca0b448c2ceb9ae0f186772823ece28e032cc0
Joel Wembo <joelotepawembo@gmail.com>
2021-05-16 23:24:11 +0800

    SQL left join update

--- MySQL/leftJOIN-2.sql
+++ MySQL/leftJOIN-2.sql
@@ -1,9 +1,43 @@
-/***Syntax
+/*Syntax
 The basic syntax of a LEFT JOIN is as follows.
-**/
+
+*/
 
 SELECT table1.column1, table2.column2...
 FROM table1
 LEFT JOIN table2
 ON table1.common_field = table2.common_field;
+
+/*      Function to
+
+Example
+Consider the following two tables,
+
+Table 1 − CUSTOMERS Table is as follows.
+
++----+----------+-----+-----------+----------+
+| ID | NAME     | AGE | ADDRESS   | SALARY   |
++----+----------+-----+-----------+----------+
+|  1 | Ramesh   |  32 | Ahmedabad |  2000.00 |
+|  2 | Khilan   |  25 | Delhi     |  1500.00 |
+|  3 | kaushik  |  23 | Kota      |  2000.00 |
+|  4 | Chaitali |  25 | Mumbai    |  6500.00 |
+|  5 | Hardik   |  27 | Bhopal    |  8500.00 |
+|  6 | Komal    |  22 | MP        |  4500.00 |
+|  7 | Muffy    |  24 | Indore    | 10000.00 |
++----+----------+-----+-----------+----------+
+Table 2 − Orders Table is as follows.
+
++-----+---------------------+-------------+--------+
+| OID | DATE                | CUSTOMER_ID | AMOUNT |
++-----+---------------------+-------------+--------+
+| 102 | 2009-10-08 00:00:00 |           3 |   3000 |
+| 100 | 2009-10-08 00:00:00 |           3 |   1500 |
+| 101 | 2009-11-20 00:00:00 |           2 |   1560 |
+| 103 | 2008-05-20 00:00:00 |           4 |   2060 |
++-----+---------------------+-------------+--------+
+
+
+*/
+
 
