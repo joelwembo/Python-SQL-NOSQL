322094c90bb20b97e9fd30c13ee4250f6fa04837
Joel Wembo <joelotepawembo@gmail.com>
2021-05-16 23:37:46 +0800

    Create SQLCommands.sql

---
+++ SQLCommands.sql
@@ -0,0 +1,7 @@
+/*  
+
+SQL is a language to operate databases; it includes database creation, deletion, fetching rows, modifying rows, etc. 
+SQL is an ANSI (American National Standards Institute) standard language, but there are many different versions of the SQL language.
+
+*/
+
